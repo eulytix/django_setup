92f8ddd53cd8a23d120b8610361a3122c6d5a3af
kocsislevente <lkocsis92@gmail.com>
2024-09-03 09:45:02 +0200

    dependency

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name='setup',
     version='0.1.0',
     packages=find_packages(),  # Automatically find and include packages
-    install_requires=["os", "sys"],  # List your dependencies here
+    install_requires=[],  # List your dependencies here
     python_requires='>=3.9',  # Specify your supported Python versions
 )
